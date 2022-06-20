246d3297e25bcd541be1ecd397387b5a8f942b9c
Salomao S <souza95salomao@gmail.com>
2022-06-20 21:05:52 +0000

    init: project scrapping

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Hellp ignaito lab. :D</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+import { theme } from "/theme.json"
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme,
+  plugins: [],
+}
