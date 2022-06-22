95ffdd48856e88399d4e08400da332826f514f13
Salomao S <souza95salomao@gmail.com>
2022-06-22 17:47:09 +0000

    update: some minor homt style adjustments

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -29,11 +29,12 @@ export function Home() {
 			>
 				<section className="lg:flex-[0.6] px-14">
 					<img
+						className="mx-auto h-10 md:mx-0"
 						src={TikoTekoLogo}
 						alt="Tiko Teko Lab logo"
 					/>
 					<h1 className="text-gray-100 text-4xl text-center lg:text-left my-16">
-						Build an <span className="text-blueish">entire application</span>, from scratch, with <span className="text-blueish">React JS</span>
+						Build an <span className="text-blue-400">entire application</span>, from scratch, with <span className="text-blue-400">React JS</span>
 					</h1>
 
 					<p className="text-gray-300 mb-16 mt-[-2rem]">In only one week you'll dominate by doing on of most used techs with high demand to acess the best job opportunities.</p>
@@ -42,7 +43,7 @@ export function Home() {
 					onSubmit={handleSubmit}
 					className="w-full lg:w-132 lg:h-fit bg-gray-600 border-0 lg:border-[1px] border-gray-300 rounded-md px-16 py-8 lg:p-4 flex flex-col gap-8"
 				>
-					<h1 className="text-gray-100">Register free!</h1>
+					<h1 className="text-gray-100 font-bold">Register free</h1>
 					<input
 						required
 						type="text"
@@ -55,7 +56,7 @@ export function Home() {
 						placeholder="Your best email"
 						className="w-full h-12 placeholder:text-gray-400 text-blue-400 indent-4 bg-gray-700"
 					/>
-					<button className="w-full h-10 bg-green-500 text-gray-100 rounded-md">ENSURE MY VAGA</button>
+					<button className="w-full h-10 bg-green-500 text-gray-100 font-bold rounded-md">ENSURE MY VAGA</button>
 				</form>
 			</main>
 			<img
