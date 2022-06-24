9020f80219417df6a9fe21cc61888a6d79fa591f
Salomao S <souza95salomao@gmail.com>
2022-06-24 14:43:33 +0000

    upfix: not found self pasted routes ate deploy second try

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-	build: {
-		rollipOptions: {
-			input: {
-				main: resolve(__dirname, 'index.html')
-			}
-		}
-	}
+  plugins: [react()]
 })
